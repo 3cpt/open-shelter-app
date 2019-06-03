55e2fbf3e35d937cea00d2a4a0f886d286efd753
andre <andrecorreiait@gmail.com>
2019-06-03 23:35:44 +0100

    initial commit

---
+++ src/OpenShelter/Models/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SQLite;
+
+namespace OpenShelter.Models
+{
+    public class Attendance
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string TaskType { get; set; }
+
+        public DateTime EnterTime { get; set; }
+
+        public DateTime ExitTime { get; set; }
+    }
+}
---
+++ src/OpenShelter/Models/Volunter.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace OpenShelter.Models
+{
+    public class Volunter
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+
+        public int AccessCode { get; set; }
+    }
+}
