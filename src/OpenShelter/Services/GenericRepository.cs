f923b3dc5a8805fa86eebe113eeb895535569e44
andre <andrecorreiait@gmail.com>
2019-06-06 23:56:26 +0100

    Create GenericRepository.cs

---
+++ src/OpenShelter/Services/GenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using System.Linq.Expressions;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OpenShelter.Services
+{
+    public abstract class GenericRepository<T> where T : new()
+    {
+        protected GenericRepository()
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(this.DbPath))
+                {
+                    conexao.CreateTable<T>();
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                // Log.Info("SQLiteEx", ex.Message);
+            }
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(this.DbPath))
+                {
+                    return conexao.Table<T>().FirstOrDefault(expression);
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                // Log.Info("SQLiteEx", ex.Message);
+                return default;
+            }
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            try
+            {
+                using (var conexao = new SQLiteConnection(this.DbPath))
+                {
+                    return conexao.Table<T>().Where(expression);
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                // Log.Info("SQLiteEx", ex.Message);
+                return null;
+            }
+        }
+
+        public string DbPath { get { return Path.Combine(Environment.CurrentDirectory, "Db", "attendances.db"); } }
+    }
+}
