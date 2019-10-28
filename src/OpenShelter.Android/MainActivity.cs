edb81403c9e579449dfa1055f88e78d8d17bd265
andre <andrecorreiait@gmail.com>
2019-10-28 21:13:10 +0000

    change icon

--- src/OpenShelter.Android/MainActivity.cs
+++ src/OpenShelter.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.OS;
 
 namespace OpenShelter.Droid
 {
-    [Activity(Label = "OpenShelter", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "OpenShelter", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
